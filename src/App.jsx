3ea085b2f303cbfe6e947f002184d582287717bd
Qasim <mianqasim065@gmail.com>
2025-07-26 05:32:18 +0500

    feat: fetch questions from local json-server API

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@
 import Main from "./main"
 import Header from "./Header"
 import "./App.css"
+import { useEffect } from "react"
+
 function App() {
+  useEffect(function(){
+    async function getQuestion() {
+      const res =await fetch("http://localhost:8000/questions");
+      const data = await res.json()
+      console.log(data);
+    }
+    getQuestion();
+  },[])
   return(
+    
     <div>
       <Header/>
       <main className="main">
          <main>
           <p>1/15</p>
-          <p>Quesion</p>
+          <p>Quesion </p>
          </main>
          
       </main>
