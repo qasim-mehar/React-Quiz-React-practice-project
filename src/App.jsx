6c94d133749ceb48281a1f99412d52e9c4de8283
Qasim <mianqasim065@gmail.com>
2025-07-26 05:13:05 +0500

    Start the project with a header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import Header from "./Header"
+import "./App.css"
+function App() {
+  return(
+    <div>
+      <Header/>
+      
+    </div>
+  )
+}
+
+export default App
