893b9a4ee1ef34415699afc049cdfff576e57a47
Qasim <mianqasim065@gmail.com>
2025-07-26 05:14:17 +0500

    Create a new main component and import it in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import { useState } from 'react'
-
+import Main from "./main"
 import Header from "./Header"
 import "./App.css"
 function App() {
   return(
     <div>
       <Header/>
-      
+      <main className="main">
+         <main>
+          <p>1/15</p>
+          <p>Quesion</p>
+         </main>
+         
+      </main>
     </div>
   )
 }
