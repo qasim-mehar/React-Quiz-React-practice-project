70ca022e65d87374555fa46a5cce919c92e639b9
Qasim <mianqasim065@gmail.com>
2025-07-30 10:32:58 +0500

    Feat: add emoji feedback on result based on score percentage

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,7 +1,25 @@
 function FinishScreen({points,totalPoints}) {
-    const percentage=(points/totalPoints)*100;
+    const percentage = Math.ceil((points / totalPoints) * 100);
+let emoji;
+
+if (percentage <= 40) {
+  emoji = "💩";
+} else if (percentage <= 50) {
+  emoji = "😕";
+} else if (percentage <= 60) {
+  emoji = "🙂";
+} else if (percentage <= 70) {
+  emoji = "😎";
+} else if (percentage <= 85) {
+  emoji = "🤩";
+} else if (percentage <= 95) {
+  emoji = "🔥";
+} else {
+  emoji = "🧠";
+}
+
     return(
-        <p className="result"> You have scored <strong>{points}</strong> out of {totalPoints} ({Math.ceil(percentage)}%)</p>
+        <p className="result"> You have scored <strong>{points}</strong> out of {totalPoints} ({Math.ceil(percentage)}%) {emoji}</p>
     )
 }
 export default FinishScreen;
