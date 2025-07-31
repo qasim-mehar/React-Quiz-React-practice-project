e0d90646939e4907e3112d546426da03cf352f37
Qasim <mianqasim065@gmail.com>
2025-07-31 12:25:01 +0500

    Feat: format timer in minutes and seconds instead of just seconds to display

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 
 function Timer({dispatch, secondsRemaining}){
+    const minutes=Math.floor(secondsRemaining/60);
+    const seconds=secondsRemaining%60;
     useEffect(function(){
         const intervelId=setInterval(function(){
             dispatch({type:"timer"})
@@ -9,7 +11,7 @@ function Timer({dispatch, secondsRemaining}){
         return ()=>clearInterval(intervelId);
     },[dispatch])
     return(
-        <div className="timer">{secondsRemaining}</div>
+        <div className="timer">{minutes} : {seconds}</div>
     )
 }
 export default Timer;
